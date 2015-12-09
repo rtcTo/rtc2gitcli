f0f4c147ab66bd86ab89f475d0ea5689a1f15003
fbuehlmann <florian@buhlmann.net>
2015-12-09 07:14:01 +0100

    Updated plugin configuration to enable plugin in rtc scm.

--- rtc2git.cli.extension/src/to/rtc/cli/migrate/git/MigrateToGitOptions.java
+++ rtc2git.cli.extension/src/to/rtc/cli/migrate/git/MigrateToGitOptions.java
@@ -18,9 +18,12 @@ public class MigrateToGitOptions implements IOptionSource {
     Options options = new Options(false);
 
     SubcommandUtil.addRepoLocationToOptions(options);
-    options.addOption(
-        new PositionalOptionDefinition(OPT_SRC_WS, "source-workspace-name", 1, 1), "name of the pre configured source workspace to change"); //$NON-NLS-1$
-    options.addOption(new PositionalOptionDefinition(OPT_DEST_WS, "destination-workspace-name", 1, 1), "destination-workspace-name"); //$NON-NLS-1$
+    options
+        .addOption(
+            new PositionalOptionDefinition(OPT_SRC_WS, "source-workspace-name", 1, 1), "name of the pre configured source RTC workspace that could follow the stream to migrate."); //$NON-NLS-1$
+    options
+        .addOption(
+            new PositionalOptionDefinition(OPT_DEST_WS, "destination-workspace-name", 1, 1), "name of the pre configured RTC workspace that holds the current state of the migration and that follows the source-workspace-name."); //$NON-NLS-1$
     return options;
   }
 }
