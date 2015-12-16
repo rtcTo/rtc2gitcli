5307ed55477a66e946ecc91078f66f20e08553c1
Otmar Humbel <ohumbel@gmail.com>
2015-12-16 19:11:36 +0100

    clarifies corner cases
    The translation used to fail with indented comment lines containing
    {patterns}.
    The translation used to fail with a line continuation on the last line.

--- rtc2git.cli.extension/src_test/to/rtc/cli/migrate/util/JazzignoreTranslatorTest.java
+++ rtc2git.cli.extension/src_test/to/rtc/cli/migrate/util/JazzignoreTranslatorTest.java
@@ -29,31 +29,12 @@ public class JazzignoreTranslatorTest {
 
 	private List<String> getJazzignoreLines() {
 		List<String> lines = new ArrayList<String>();
+		// leading white space is intentional
 		lines.add("### Jazz Ignore 0");
-		lines.add("# Ignored files and folders will not be committed, but may be modified during");
-		lines.add("# accept or update.");
-		lines.add("# - Ignore properties should contain a space separated list of filename patterns.");
-		lines.add("# - Each pattern is case sensitive and surrounded by braces ('{' and '}').");
-		lines.add("# - \"*\" matches zero or more characters.");
-		lines.add("# - \"?\" matches a single character.");
-		lines.add("# - The pattern list may be split across lines by ending the line with a");
-		lines.add("#     backslash and starting the next line with a tab.");
-		lines.add("# - Patterns in core.ignore prevent matching resources in the same");
-		lines.add("#     directory from being committed.");
-		lines.add("# - Patterns in core.ignore.recursive matching resources in the current");
-		lines.add("#     directory and all subdirectories from being committed.");
-		lines.add("# - The default value of core.ignore.recursive is *.class");
-		lines.add("# - The default value for core.ignore is bin");
-		lines.add("#");
-		lines.add("# To ignore shell scripts and hidden files in this subtree:");
-		lines.add("#     e.g: core.ignore.recursive = {*.sh} {\\.*}");
-		lines.add("#");
-		lines.add("# To ignore resources named 'bin' in the current directory (but allow");
-		lines.add("#  them in any sub directorybelow):");
-		lines.add("#     e.g: core.ignore.recursive = {*.sh} {\\.*}");
-		lines.add("#");
-		lines.add("# NOTE: modifying ignore files will not change the ignore status of");
-		lines.add("#     Eclipse derived resources.");
+		lines.add("   #    The pattern list may be split across lines");
+		lines.add("  #    e.g: core.ignore.recursive = {*.sh} {\\.*}");
+		lines.add(" #    some text");
+		lines.add("#    some text");
 		lines.add("");
 		lines.add("   core.ignore.recursive = {*.class} {*.so} \\");
 		lines.add("       {*.pyc}");
@@ -64,7 +45,7 @@ public class JazzignoreTranslatorTest {
 		lines.add("  {.settings} \\");
 		lines.add("  {bin} \\");
 		lines.add("  {dist} \\");
-		lines.add("  {a?c} \\");
+		lines.add("  {a?c} \\"); // no next line after line continuation
 		return lines;
 	}
 
